1ba96efd86b90355aba48b7fd06dc90553b240d9
akalmannakarmi <akalnakarmi1@gmail.com>
2025-12-20 10:42:55 +0545

    feat: group required mixin and example with user list

--- user/views.py
+++ user/views.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import login
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, ListView
 from django.urls import reverse_lazy
 
 
 from .forms import RegisterForm
 from .models import User
+from .mixins import AdminRequiredMixin
 
 
 class RegisterView(CreateView):
@@ -36,3 +37,18 @@ class ProfileView(LoginRequiredMixin, TemplateView):
         ctx = super().get_context_data(**kwargs)
         ctx["user"] = self.request.user
         return ctx
+
+
+class UsersListView(AdminRequiredMixin, ListView):
+    model = User
+    template_name = "user/user-list.html"
+    context_object_name = "users"
+    paginate_by = 10
+    ordering = ["-date_joined"]
+
+    def get_queryset(self):
+        return (
+            User.objects
+            .select_related()
+            .prefetch_related("groups")
+        )
