d080b25ccac1fa605379fd1f46da5935d361e929
akalmannakarmi <akalnakarmi1@gmail.com>
2025-12-20 17:45:15 +0545

    docker add groups on start

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,8 @@ python manage.py collectstatic --noinput
 echo "Running migrations..."
 python manage.py migrate --noinput
 
+echo "Running add groups"
+python manage.py add_groups
+
 echo "Starting server..."
 exec "$@"
