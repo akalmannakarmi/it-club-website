a1e62b65a3b480f1e0a50ecc785bb87a95b83cef
akalmannakarmi <akalnakarmi1@gmail.com>
2026-01-03 19:13:01 +0545

    remove the contab prev attempt junk

--- config/celery.py
+++ config/celery.py
@@ -1,4 +1,3 @@
-from celery.schedules import crontab
 from celery import Celery
 import os
 
@@ -7,12 +6,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 app = Celery("config")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
-
-
-CELERY_BEAT_SCHEDULE = {
-    "scrape-task": {
-        "task": "events.tasks.scrape_task",
-        #"schedule": crontab(hour=0, minute=0), 
-        "schedule": crontab(minute='*'), # TO run every minute to test
-    },
-}
