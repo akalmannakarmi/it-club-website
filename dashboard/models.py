5dc1a5dd39dd5d0710f059e6af8faad395a2e023
Ashok oli <ashokoli642@gmail.com>
2026-01-13 07:11:38 +0545

    user CRUD

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 
 
 class Member(models.Model):
@@ -13,9 +13,9 @@ class Member(models.Model):
          ]
 
     name = models.CharField(max_length=150,default="")
-    faculty = models.CharField(max_length=10, choices=FACULTY_CHOICES)
-    batch=models.CharField(max_length=10)
+    semester=models.CharField()
     phone = models.CharField(max_length=15)
+    faculty = models.CharField(max_length=10, choices=FACULTY_CHOICES)
     interested_topics = models.TextField(help_text="Comma separated topics")
     
     
