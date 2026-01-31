dcf46fe415b0c5af1f0b65c9f4eaca60a7da52ec
akalmannakarmi <akalnakarmi1@gmail.com>
2026-01-31 16:25:01 +0545

    changed login and signup pages

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import IndexView, AnnouncementsView, EventsView, ResourceView
 
 urlpatterns = [
-    path("", IndexView.as_view(), name="index"),
+    path("", IndexView.as_view(), name="home"),
     path("announcement/", AnnouncementsView.as_view(), name="announcemetn"),
     path("events/", EventsView.as_view(), name="events"),
     path("resource/", ResourceView.as_view(), name="resources"),
